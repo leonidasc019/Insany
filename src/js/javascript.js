143fea70f2c64a0811c6c6ecb3c303222ae23cb4
Leônidas <leonidas19992011@hotmail.com>
2023-07-15 22:52:52 -0300

    init top bar

---
+++ src/js/javascript.js
@@ -0,0 +1,16 @@
+function showText() {
+    document.getElementById("cadButton").innerHTML = "<span style='color: #1D63FF;'>Cadastre-se gratuitamente →</span>"; 
+  }
+
+function reset() {
+    document.getElementById("cadButton").innerHTML = "<span style='color: #1D63FF;'>Cadastre-se gratuitamente</span>"; 
+  }
+
+/* Inicio função para Acess */
+function showAcess() {
+    document.getElementById("acButton").innerHTML = "<span style='color: #1D63FF;'>Acessar</span>"; 
+  }
+
+function resetAcess() {
+    document.getElementById("acButton").innerHTML = ""; 
+  }
