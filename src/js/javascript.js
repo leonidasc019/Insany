929fe17b32e6bfc44411e4ed3c8da10c42ceb401
Leônidas <leonidas19992011@hotmail.com>
2023-07-19 00:09:25 -0300

    Section How it Work feita

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -24,20 +24,3 @@ function resetHero() {
   document.getElementById("hero-c").innerHTML = ""; 
 }
 
-  /* Função de lop de 0 até o número da página de feedback... */
-  function animateValue(obj, start, end, duration) {
-    let startTimestamp = null;
-    const step = (timestamp) => {
-      if (!startTimestamp) startTimestamp = timestamp;
-      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      obj.innerHTML = Math.floor(progress * (end - start) + start);
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      }
-    };
-    window.requestAnimationFrame(step);
-  }
-  
-  const obj = document.getElementById("value");
-  animateValue(obj, 100, 0, 5000);
-  
